844377447fd6f7787c875433bf5151aac1a5c20d
Pangduck <kl.park@mantech.co.kr>
2024-07-04 14:43:43 +0900

    modify host

--- k8s_repo_rhel.sh
+++ k8s_repo_rhel.sh
@@ -3,7 +3,7 @@
 
 # hosts config
 
-cat <<EOF >>/etc/hosts
+cat <<EOF >/etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
 10.60.200.60 acc-master1 
